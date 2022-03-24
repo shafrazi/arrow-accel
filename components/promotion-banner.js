95dbf89f2729bd75a2ee751070fbedf067e659e9
shafrazi <shafrazi@live.com>
2022-03-24 18:56:54 +0530

    add progress image and section

--- components/promotion-banner.js
+++ components/promotion-banner.js
@@ -21,6 +21,24 @@ export default function PromotionBanner() {
           </p>
         </div>
       </div>
+      <div className="flex flex-col md:flex-row w-full bg-gray-200">
+        <div className="w-full md:w-1/2 px-4 md:px-[10rem] py-4 md:py-[10rem]">
+          <h2 className="tracking-wide text-lg md:text-3xl uppercase">
+            and accelerating progress
+          </h2>
+          <p className="tracking-wide text-lg md:text-2xl mt-10">
+            Our decisions are based on data and research, leaning on our
+            extensive experience, working for companies and organizations who
+            have their ambitions set beyond the horizon.
+          </p>
+          <p className="tracking-wide text-lg md:text-2xl mt-10">
+            We connect sharp minds to accelerate extraordinary development.
+          </p>
+        </div>
+        <div className="w-full md:w-1/2 py-12 flex justify-center items-center">
+          <img src="/images/progress.png" className="w-full" />
+        </div>
+      </div>
     </div>
   );
 }

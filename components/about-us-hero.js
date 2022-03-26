545a9184f418c44fa2d3e17f6120fbecd539a5d5
shafrazi <shafrazi@live.com>
2022-03-26 15:59:35 +0530

    fix about us hero

--- components/about-us-hero.js
+++ components/about-us-hero.js
@@ -11,17 +11,17 @@ export default function AboutUsHero() {
       >
         <source src="/videos/about-us-video.mp4" type="video/mp4" />
       </video>
-      <div className="flex w-1/2 h-[20rem] md:h-[30rem] lg:h-[40rem] bg-red-900 bg-opacity-25 absolute bottom-[0rem] left-6 md:left-[5rem]">
-        <div className="mx-20 mt-36 text-center">
+      <div className="flex w-full md:w-1/2 h-[20rem] md:h-[30rem] lg:h-[40rem] bg-red-900 bg-opacity-25 absolute bottom-[0rem] md:left-[5rem]">
+        <div className="flex flex-col justify-center items-center mx-5 marker:md:mx-10 lg:mx-20 mt-4 md:mt-18 lg:mt-36 text-center">
           <p
-            className="tracking-wide text-2xl text-center font-semibold text-white mb-10"
+            className="tracking-wide text-lg md:text-xl lg:text-2xl text-center font-semibold text-white mb-2 md:mb-5 lg:mb-10"
             style={{ fontFamily: "OpenSauceOne", fontWeight: "normal" }}
           >
             We are consultants for brands, entertainment professionals,
             start-ups and artists, ready to create, develop or reinvigorate your
             business.
           </p>
-          <Image src="/images/logo-icon-white.png" width={250} height={200} />
+          <img src="/images/logo-icon-white.png" className="w-1/3" />
         </div>
       </div>
     </div>

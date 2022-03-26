0be22e6e67b021486fb65909404690e923e447d8
shafrazi <shafrazi@live.com>
2022-03-26 22:47:59 +0530

    fix focus intro responsiveness

--- components/focus-intro.js
+++ components/focus-intro.js
@@ -1,24 +1,33 @@
 export default function FocusIntro() {
   return (
     <div className="flex flex-col w-full tracking-wide">
-      <div className="flex w-full my-[8rem]">
-        <div
-          className="w-1/2 px-[10rem] mt-6 tracking-wider leading-loose"
-          style={{
-            fontFamily: "OpenSauceOne",
-            fontWeight: "normal",
-          }}
-        >
-          <p className="text-2xl" style={{ lineHeight: 1.6 }}>
-            Our current economic system rewards exploitation and harm. Our focus
-            lies on how to transition to nature-based solutions, bio-mimetic
-            technologies and transition analysis which can be applied to any
-            industry.
-          </p>
+      <div className="w-full flex flex-col my-4 md:my-16 lg:my-[8rem]">
+        <div className="flex w-full">
+          <div
+            className="w-full md:w-3/4 lg:w-2/3 px-5 md:pl-[3rem] lg:pl-[5rem] mt-6 tracking-wider md:leading-loose text-base md:text-xl lg:text-2xl"
+            style={{ fontFamily: "OpenSauceOne" }}
+          >
+            <p className="" style={{ lineHeight: 1.6 }}>
+              Our current economic system rewards
+            </p>
+            <p className="" style={{ lineHeight: 1.6 }}>
+              exploitation and harm. Our focus lies on how
+            </p>
+            <p className="" style={{ lineHeight: 1.6 }}>
+              to transition to nature-based solutions, bio-
+            </p>
+            <p className="" style={{ lineHeight: 1.6 }}>
+              mimetic technologies and transition analysis
+            </p>
+            <p className="" style={{ lineHeight: 1.6 }}>
+              which can be applied to any industry.
+            </p>
+          </div>
         </div>
       </div>
-      <div className="w-full flex flex-col md:flex-row bg-black text-white px-[10rem] py-[8rem] text-2xl leading-relaxed">
-        <div className="w-1/2 pr-16">
+
+      <div className="w-full flex flex-col lg:flex-row bg-black text-white px-10 lg:px-[10rem] py-6 lg:py-[8rem] text-lg lg:text-2xl leading-relaxed">
+        <div className="w-full lg:w-1/2 py-5 lg:py-0 lg:pr-16">
           <p className="">
             <span className="font-bold">Transitions</span> have social and
             communicative challenges constituting behavioral change, change
@@ -26,7 +35,7 @@ export default function FocusIntro() {
             seems as opportunities abound.
           </p>
         </div>
-        <div className="w-1/2 pl-16">
+        <div className="w-full lg:w-1/2 py-5 lg:py-0 lg:pl-16">
           <p className="">
             <span className="font-bold">Transformations</span> produce
             real-world outcomes which vary greatly by industry and between
@@ -35,8 +44,8 @@ export default function FocusIntro() {
           </p>
         </div>
       </div>
-      <div className="w-full flex flex-col justify-center items-center py-[8rem]">
-        <p className="text-2xl text-center w-1/3 leading-relaxed">
+      <div className="w-full flex flex-col justify-center items-center py-6 lg:py-[8rem]">
+        <p className="text-xl lg:text-2xl text-center w-full px-10 md:px-0 md:w-1/2 lg:w-1/3 leading-relaxed">
           To reach your goals, you need to translate your mission effectively,
           with an understanding of your audience, the and you are building and
           why the world should pay attention.

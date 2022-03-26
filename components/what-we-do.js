af8c9546fa3f8b17b185d72160604b8c64dc7136
shafrazi <shafrazi@live.com>
2022-03-27 00:12:00 +0530

    add inner container to what we do sections

--- components/what-we-do.js
+++ components/what-we-do.js
@@ -5,103 +5,118 @@ export default function WhatWeDo() {
         <img src="/images/build-it.png" className="w-full" />
       </div>
       <div className="flex flex-col lg:flex-row w-full mt-6 lg:mt-16">
-        <div className="w-full lg:w-1/2 px-4 md:px-12 lg:px-[10rem] py-12 lg:py-[5rem]">
-          <h2 className="tracking-wide text-xl md:text-3xl font-bold">
-            Energy transition
-          </h2>
-          <p className="tracking-wide text-lg md:text-2xl mt-5 lg:mt-10">
-            We are commited to drive the energy transition past Net Zero targets
-            to meet 1.5 ºC sustainability goals of the Paris Agreement. The
-            transition needs to be accelerated as we are running our of time.
-          </p>
-          <p className="tracking-wide text-lg md:text-2xl mt-5 lg:mt-10">
-            At ARROW ACCEL, we create compelling content for companies and
-            foundations that actively address contemporary challenges with
-            breakthrough solutions that contribute to protecting nature and life
-            on Earth.
-          </p>
+        <div className="flex flex-col w-full lg:w-1/2 justify-center items-center">
+          <div className="w-full lg:w-10/12 py-6 md:py-10 lg:py-16 px-4 md:px-10 lg:px-0">
+            <h2 className="tracking-wide text-xl md:text-3xl font-bold">
+              Energy transition
+            </h2>
+            <p className="tracking-wide text-lg md:text-2xl mt-5 lg:mt-10">
+              We are commited to drive the energy transition past Net Zero
+              targets to meet 1.5 ºC sustainability goals of the Paris
+              Agreement. The transition needs to be accelerated as we are
+              running our of time.
+            </p>
+            <p className="tracking-wide text-lg md:text-2xl mt-5 lg:mt-10">
+              At ARROW ACCEL, we create compelling content for companies and
+              foundations that actively address contemporary challenges with
+              breakthrough solutions that contribute to protecting nature and
+              life on Earth.
+            </p>
+          </div>
         </div>
-        <div className="w-full lg:w-1/2 px-4 md:px-12 lg:px-[10rem] py-12 lg:py-[5rem] bg-black text-white">
-          <h2 className="tracking-wide text-xl md:text-3xl font-bold">
-            Art & entertainment
-          </h2>
-          <p className="tracking-wide text-lg md:text-2xl mt-5 lg:mt-10">
-            As a brand, collective or independent artist, in order to succeed
-            you must master both the creative and the strategic side of your
-            business.
-          </p>
-          <p className="tracking-wide text-lg md:text-2xl mt-5 lg:mt-10">
-            We work for and with artists, and creative entrepreneurs to create
-            and promote commercially viable new business, products and projects,
-            using keen strategic and marketing insight and powerful design.
-          </p>
+        <div className="flex flex-col w-full lg:w-1/2 justify-center items-center bg-black text-white">
+          <div className="w-full lg:w-10/12 py-6 md:py-10 lg:py-16 px-4 md:px-10 lg:px-0">
+            <h2 className="tracking-wide text-xl md:text-3xl font-bold">
+              Art & entertainment
+            </h2>
+            <p className="tracking-wide text-lg md:text-2xl mt-5 lg:mt-10">
+              As a brand, collective or independent artist, in order to succeed
+              you must master both the creative and the strategic side of your
+              business.
+            </p>
+            <p className="tracking-wide text-lg md:text-2xl mt-5 lg:mt-10">
+              We work for and with artists, and creative entrepreneurs to create
+              and promote commercially viable new business, products and
+              projects, using keen strategic and marketing insight and powerful
+              design.
+            </p>
+          </div>
         </div>
       </div>
       <div className="flex flex-col-reverse lg:flex-row w-full mt-6 lg:mt-16">
-        <div className="w-full lg:w-1/2 px-4 md:px-12 lg:px-[10rem] py-12 lg:py-[5rem] bg-black text-white">
-          <h2 className="tracking-wide text-xl md:text-3xl font-bold">
-            Industrial innovation
-          </h2>
-          <p className="tracking-wide text-lg md:text-2xl mt-10">
-            We accelerate industrial innovation by bringing solutions to markets
-            that solve transitional challenges in the energy market,
-            transportation space and food sectors.
-          </p>
-          <p className="tracking-wide text-lg md:text-2xl mt-10">
-            We support inspiring moonshot projects and entrepreneurs and
-            foundations that dare to think differently.
-          </p>
-          <p className="tracking-wide text-lg md:text-2xl mt-10">
-            Independent thinkers change the world we live in, with disruptive
-            industrial innovations.
-          </p>
+        <div className="flex flex-col w-full lg:w-1/2 justify-center items-center bg-black text-white">
+          <div className="w-full lg:w-10/12 py-6 md:py-10 lg:py-16 px-4 md:px-10 lg:px-0">
+            <h2 className="tracking-wide text-xl md:text-3xl font-bold">
+              Industrial innovation
+            </h2>
+            <p className="tracking-wide text-lg md:text-2xl mt-10">
+              We accelerate industrial innovation by bringing solutions to
+              markets that solve transitional challenges in the energy market,
+              transportation space and food sectors.
+            </p>
+            <p className="tracking-wide text-lg md:text-2xl mt-10">
+              We support inspiring moonshot projects and entrepreneurs and
+              foundations that dare to think differently.
+            </p>
+            <p className="tracking-wide text-lg md:text-2xl mt-10">
+              Independent thinkers change the world we live in, with disruptive
+              industrial innovations.
+            </p>
+          </div>
         </div>
-        <div className="w-full lg:w-1/2 px-4 md:px-12 lg:px-[10rem] py-12 lg:py-[5rem]">
-          <h2 className="tracking-wide text-xl md:text-3xl font-bold">
-            Future technologies
-          </h2>
-          <p className="tracking-wide text-lg md:text-2xl mt-10">
-            The world around us is rapidly changing and a clear view of what the
-            future holds, is diluted by every deeper avenue that technological
-            advance takes us into. It can be hard to keep up, let alone to stay
-            ahead. We are ahead of the tech curve.
-          </p>
-          <p className="tracking-wide text-lg md:text-2xl mt-10">
-            Our background in innovative media and fintech spans two decades.
-            Our knowledge pool covers banks, consultants, insurers,
-            cybersecurity, digital asset trading, biomimetic development and
-            fintech companies.
-          </p>
+        <div className="flex flex-col w-full lg:w-1/2 justify-center items-center">
+          <div className="w-full lg:w-10/12 py-6 md:py-10 lg:py-16 px-4 md:px-10 lg:px-0">
+            <h2 className="tracking-wide text-xl md:text-3xl font-bold">
+              Future technologies
+            </h2>
+            <p className="tracking-wide text-lg md:text-2xl mt-10">
+              The world around us is rapidly changing and a clear view of what
+              the future holds, is diluted by every deeper avenue that
+              technological advance takes us into. It can be hard to keep up,
+              let alone to stay ahead. We are ahead of the tech curve.
+            </p>
+            <p className="tracking-wide text-lg md:text-2xl mt-10">
+              Our background in innovative media and fintech spans two decades.
+              Our knowledge pool covers banks, consultants, insurers,
+              cybersecurity, digital asset trading, biomimetic development and
+              fintech companies.
+            </p>
+          </div>
         </div>
       </div>
       <div className="flex flex-col lg:flex-row w-full mt-6 lg:mt-16">
-        <div className="w-full lg:w-1/2 px-4 md:px-12 lg:px-[10rem] py-12 lg:py-[5rem]">
-          <h2 className="tracking-wide text-xl md:text-3xl font-bold">
-            Investment readiness
-          </h2>
-          <p className="tracking-wide text-lg md:text-2xl mt-10">
-            Through our global network we are able to make introductions to
-            capital sources for project of any size and strategy, growth equity,
-            funds and philantropy for sustainable investment solutions.
-          </p>
-          <p className="tracking-wide text-lg md:text-2xl mt-10">
-            With the right marketing & sales collateral we work on sharpening
-            your aim to shoot for the moon.
-          </p>
+        <div className="flex flex-col w-full lg:w-1/2 justify-center items-center">
+          <div className="w-full lg:w-10/12 py-6 md:py-10 lg:py-16 px-4 md:px-10 lg:px-0">
+            <h2 className="tracking-wide text-xl md:text-3xl font-bold">
+              Investment readiness
+            </h2>
+            <p className="tracking-wide text-lg md:text-2xl mt-10">
+              Through our global network we are able to make introductions to
+              capital sources for project of any size and strategy, growth
+              equity, funds and philantropy for sustainable investment
+              solutions.
+            </p>
+            <p className="tracking-wide text-lg md:text-2xl mt-10">
+              With the right marketing & sales collateral we work on sharpening
+              your aim to shoot for the moon.
+            </p>
+          </div>
         </div>
-        <div className="w-full lg:w-1/2 px-4 md:px-12 lg:px-[10rem] py-12 lg:py-[5rem] bg-black text-white">
-          <h2 className="tracking-wide text-xl md:text-3xl font-bold">
-            Transformational technology
-          </h2>
-          <p className="tracking-wide text-lg md:text-2xl mt-10">
-            Digital transformation is a key word in marketing toolbox nowadays.
-            To us, it's more than just a phrase.
-          </p>
-          <p className="tracking-wide text-lg md:text-2xl mt-10">
-            We are digital transformation leaders, able to tokenize real-world
-            and digital assets. Our AR/VR/Metaverse and blended experiences make
-            your projects come to life.
-          </p>
+        <div className="flex flex-col w-full lg:w-1/2 justify-center items-center bg-black text-white">
+          <div className="w-full lg:w-10/12 py-6 md:py-10 lg:py-16 px-4 md:px-10 lg:px-0">
+            <h2 className="tracking-wide text-xl md:text-3xl font-bold">
+              Transformational technology
+            </h2>
+            <p className="tracking-wide text-lg md:text-2xl mt-10">
+              Digital transformation is a key word in marketing toolbox
+              nowadays. To us, it's more than just a phrase.
+            </p>
+            <p className="tracking-wide text-lg md:text-2xl mt-10">
+              We are digital transformation leaders, able to tokenize real-world
+              and digital assets. Our AR/VR/Metaverse and blended experiences
+              make your projects come to life.
+            </p>
+          </div>
         </div>
       </div>
       <div className="w-full flex flex-col lg:flex-row bg-black text-white px-6 md:px-10 lg:px-[10rem] py-10 md:py-14 lg:py-[8rem] text-lg md:text-xl lg:text-2xl leading-relaxed mt-16">

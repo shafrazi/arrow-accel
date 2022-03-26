78100b532524a1cc0d6f3af44cf47a9814202e25
shafrazi <shafrazi@live.com>
2022-03-26 22:07:40 +0530

    fix partners component

--- components/partners.js
+++ components/partners.js
@@ -1,33 +1,35 @@
 export default function Partners() {
   return (
     <div className="flex flex-col w-full tracking-wide">
-      <div className="w-full flex flex-col bg-black text-white justify-center items-center py-[8rem]">
-        <h2 className="text-white text-5xl font-bold">Our partners</h2>
-        <p className="text-white text-2xl mt-12 text-center w-2/5">
+      <div className="w-full flex flex-col bg-black text-white justify-center items-center py-6 md:py-12 lg:py-[8rem]">
+        <h2 className="text-white text-2xl md:text-2xl lg:text-5xl font-bold">
+          Our partners
+        </h2>
+        <p className="text-white text-lg md:text-2xl mt-5 md:mt-8 lg:mt-12 text-center w-full px-4 md:px-0 md:w-1/2 lg:w-2/5">
           Together with our partners, we organize teams, develop a strategy and
           create campaigns designed to capture attention and inspire
           imagination.
         </p>
-        <p className="text-white text-2xl mt-12 text-center w-2/5">
+        <p className="text-white text-lg md:text-2xl mt-5 md:mt-8 lg:mt-12 text-center w-full px-4 md:px-0 md:w-1/2 lg:w-2/5">
           They work to the best of their ability, transforming your vision into
           the real and virtual world.
         </p>
       </div>
-      <div className="flex flex-col md:flex-row w-full px-[10rem] my-12">
-        <div className="w-1/3 flex flex-col py-[10rem] text-xl text-center pr-10">
+      <div className="flex flex-col md:flex-row w-full px-4 lg:px-[10rem] my-5 lg:my-12">
+        <div className="w-full px-6 lg:px-8 lg:w-1/3 flex flex-col py-10 lg:py-[10rem] text-xl text-center lg:pr-10">
           T M M is a design studio which delivers graphic design and visual
           communication to clients and collaborators who are looking for
           engaging and strong output that is able to challenge expectations and
           goals.
         </div>
-        <div className="w-1/3 flex flex-col py-[10rem] text-xl text-center bg-neutral-100 px-10">
+        <div className="w-full px-4 lg:px-8 lg:w-1/3 flex flex-col py-10 lg:py-[10rem] text-xl text-center bg-neutral-100">
           K-line achieves objectives through measurable and accountable
           marketing. They work as an extension of your team by filling the gaps
           with the right specialists. Our full-service capabilities allow us to
           provide professional recommendations a;nd solutions across every
           aspect of your marketing.
         </div>
-        <div className="w-1/3 flex flex-col py-[10rem] text-xl text-center pl-10">
+        <div className="w-full px-4 lg:px-8 lg:w-1/3 flex flex-col py-10 lg:py-[10rem] text-xl text-center lg:pr-10">
           Neely & Daughters is a think tank, creative incubator, and studio
           where visionary innovations are born. Neely & Daughters are renegade
           creatives producing revolutionary content embraced by millions over

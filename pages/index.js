9d95bed7574d898fa0b6cd0c7a3a5f4b23b724cd
shafrazi <shafrazi@live.com>
2022-03-28 00:21:09 +0530

    remove redundant components in index.js

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,10 @@ import Head from "next/head";
 import Header from "../components/header";
 import Hero from "../components/hero";
 import HeroTwo from "../components/hero2";
-import HeroThree from "../components/hero3";
 import Footer from "../components/footer-copy";
 import Layout from "../components/layout";
-import Image from "next/image";
-import homePageCanvasImg from "../public/images/home-page-canvas.png";
 import Stats from "../components/stats";
 import CallToActionBanner from "../components/call-to-action-banner";
-import GetToKnowBanner from "../components/get-to-know-banner";
 import Clients from "../components/clients";
 import GetToKnow from "../components/get-to-know";
 

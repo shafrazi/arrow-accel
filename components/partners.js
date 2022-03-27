dd917fd3551069351702adf7a44317cec787445e
shafrazi <shafrazi@live.com>
2022-03-27 15:46:31 +0530

    fix partner upper section

--- components/partners.js
+++ components/partners.js
@@ -1,19 +1,27 @@
 export default function Partners() {
   return (
     <div className="flex flex-col w-full tracking-wide">
-      <div className="w-full flex flex-col bg-black text-white justify-center items-center py-6 md:py-12 lg:py-[8rem]">
-        <h2 className="text-white text-2xl md:text-2xl lg:text-5xl font-bold">
-          Our partners
-        </h2>
-        <p className="text-white text-lg md:text-2xl mt-5 md:mt-8 lg:mt-12 text-center w-full px-4 md:px-0 md:w-1/2 lg:w-2/5">
-          Together with our partners, we organize teams, develop a strategy and
-          create campaigns designed to capture attention and inspire
-          imagination.
-        </p>
-        <p className="text-white text-lg md:text-2xl mt-5 md:mt-8 lg:mt-12 text-center w-full px-4 md:px-0 md:w-1/2 lg:w-2/5">
-          They work to the best of their ability, transforming your vision into
-          the real and virtual world.
-        </p>
+      <div className="w-full flex flex-col bg-black text-white justify-center items-center py-8 md:py-10 lg:py-[6rem]">
+        <div className="flex flex-col w-full px-6 md:px-0 lg:w-2/3 text-center leading-relaxed">
+          <h2 className="text-white text-2xl md:text-2xl lg:text-3xl font-bold">
+            Our partners
+          </h2>
+          <p className="text-white text-lg mt-4 md:mt-6 lg:mt-12 md:text-2xl w-full md:px-0">
+            Together with our partners, we organize teams,
+          </p>
+          <p className="text-white text-lg md:text-2xl w-full md:px-0">
+            develop a strategy and create campaigns designed to
+          </p>
+          <p className="text-white text-lg md:text-2xl w-full md:px-0">
+            capture attention and inspire imagination.
+          </p>
+          <p className="text-white mt-3 md:mt-5 lg:mt-10 text-lg md:text-2xl w-full md:px-0">
+            They work to the best of their ability, transforming
+          </p>
+          <p className="text-white text-lg md:text-2xl w-full md:px-0">
+            your vision into the real and virtual world.
+          </p>
+        </div>
       </div>
       <div className="flex flex-col md:flex-row w-full px-4 lg:px-[10rem] my-5 lg:my-12">
         <div className="w-full px-6 lg:px-8 lg:w-1/3 flex flex-col py-10 lg:py-[10rem] text-xl text-center lg:pr-10">

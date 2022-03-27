9110991f09773c94b6e029d6c1f4e41601363489
shafrazi <shafrazi@live.com>
2022-03-27 15:12:14 +0530

    fix promotion banner upper section

--- components/promotion-banner.js
+++ components/promotion-banner.js
@@ -1,24 +1,26 @@
 export default function PromotionBanner() {
   return (
-    <div className="flex flex-col w-full tracking-wide">
+    <div className="flex flex-col w-full tracking-wide mt-5 md:mt-10 lg:mt-[5rem]">
       <div className="flex flex-col-reverse lg:flex-row w-full">
-        <div className="w-full lg:w-1/2 py-12 flex justify-center items-center">
+        <div className="w-full lg:w-1/2 flex">
           <img src="/images/promotion-idea.png" className="w-full" />
         </div>
-        <div className="w-full lg:w-1/2 px-4 md:px-6 lg:px-[10rem] py-4 md:py-12 lg:py-[10rem]">
-          <h2 className="tracking-wide text-lg md:text-3xl uppercase text-center lg:text-left">
-            Promoting good ideas
-          </h2>
-          <p className="tracking-wide text-lg md:text-2xl mt-10">
-            We promote products, ideas and companies, that inspire and
-            prioritize the best interest of the world and the quality of peoples
-            lives.
-          </p>
-          <p className="tracking-wide text-lg md:text-2xl mt-10">
-            We are set on delivering you the means to identify, inform, engage
-            with and activating your target audience. Our aim is to shorten,
-            sharpen and refine the trajectory to your brightest goals.
-          </p>
+        <div className="w-full lg:w-1/2 flex flex-col justify-center items-center">
+          <div className="w-full lg:w-10/12 py-6 md:py-10 lg:py-16 px-4 md:px-10 lg:px-0">
+            <h2 className="tracking-wide text-lg md:text-3xl uppercase text-center lg:text-left">
+              Promoting good ideas
+            </h2>
+            <p className="tracking-wide text-lg md:text-2xl mt-10">
+              We promote products, ideas and companies, that inspire and
+              prioritize the best interest of the world and the quality of
+              peoples lives.
+            </p>
+            <p className="tracking-wide text-lg md:text-2xl mt-10">
+              We are set on delivering you the means to identify, inform, engage
+              with and activating your target audience. Our aim is to shorten,
+              sharpen and refine the trajectory to your brightest goals.
+            </p>
+          </div>
         </div>
       </div>
       <div className="flex flex-col lg:flex-row w-full bg-gray-200">

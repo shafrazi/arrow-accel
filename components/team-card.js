4a0801a9067c852f123047c4e6690f884f299b3d
shafrazi <shafrazi@live.com>
2022-03-27 23:43:22 +0530

    minor adjustments

--- components/team-card.js
+++ components/team-card.js
@@ -6,7 +6,7 @@ export default function TeamCard({
   description,
 }) {
   return (
-    <div className="w-full flex md:w-1/2 justify-center items-center">
+    <div className="w-full px-4 md:px-0 mt-5 md:mt-0 flex md:w-1/2 justify-center items-center">
       <div className="w-full md:w-10/12 flex flex-col">
         <div className="w-full flex space-x-5">
           <div className="flex items-center w-3/12 md:w-4/12 lg:w-3/12">

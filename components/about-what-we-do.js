9ff8c67030c41bb05c5608d2b9da6ef1f4d93e77
shafrazi <shafrazi@live.com>
2022-03-27 14:42:37 +0530

    fix foundation banner

--- components/about-what-we-do.js
+++ components/about-what-we-do.js
@@ -1,29 +1,29 @@
 export default function AboutWhatWeDo() {
   return (
-    <div className="flex flex-col w-full tracking-wide mt-3 md:mt-[2rem] lg:mt-[5rem] mb-[5rem]">
-      <div className="w-full flex lg:mx-0 flex-col lg:flex-row">
-        <div className="w-full lg:w-1/2 lg:py-12 flex lg:pr-16 pb-16">
+    <div className="flex flex-col w-full tracking-wide">
+      <div className="w-full flex lg:mx-0 flex-col lg:flex-row mt-10 md:mt-14 lg:mt-[8rem]">
+        <div className="w-full lg:w-1/2 flex">
           <img
             src="/images/what-we-do.png"
             className="px-4 md:px-8 lg:px-0 w-full"
           />
         </div>
-        <div className="w-full px-4 md:px-8 lg:px-0 lg:w-1/2 flex flex-col justify-center lg:pr-[10rem]">
+        <div className="w-full px-4 my-4 md:my-10 lg:my-0 lg:py-0 md:px-8 lg:px-12 lg:w-1/2 flex flex-col justify-center">
           <h2 className="tracking-wide text-lg md:text-3xl uppercase">
             What we do
           </h2>
-          <p className="tracking-wide text-base md:text-2xl mt-4 lg:mt-10">
+          <p className="tracking-wide text-base md:text-2xl mt-4 lg:mt-8">
             We improve advertising campaigns, creative ideas or other art
             projects and balance the commercial viability of a project against
             the goals of our clients.
           </p>
-          <p className="tracking-wide text-base md:text-2xl mt-4 lg:mt-10">
+          <p className="tracking-wide text-base md:text-2xl mt-4 lg:mt-8">
             We create concepts, shape ideas, form teams and together, we move
             them to where they need to be.
           </p>
-          <p className="tracking-wide text-base md:text-2xl mt-4 lg:mt-10">
+          <p className="tracking-wide text-base md:text-2xl mt-4 lg:mt-8">
             We partner with the brands of the future, innovative entrepreneurs,
-            change-makers, climatechange leaders and visionary entertainment
+            change-makers, climate change leaders and visionary entertainment
             professionals committed to maximizing their real-life impact and
             contribution to a sustainable future.
           </p>
--- components/foundation-banner.js
+++ components/foundation-banner.js
@@ -1,50 +1,60 @@
 export default function FoundationBanner() {
   return (
-    <div className="flex flex-col w-full tracking-wide">
+    <div className="flex flex-col w-full tracking-wide my-4 md:my-10 lg:my-16">
       <div className="w-full flex flex-col-reverse  lg:flex-row">
-        <div className="w-full lg:w-1/2 px-4 lg:px-[10rem] py-10 lg:py-[10rem]">
-          <p className="tracking-wide text-lg md:text-2xl">
-            ARROW ACCEL is built on our core principle of placing our clients'​
-            interests first. Our intrinsic values represent integrity, prudency,
-            tenacity, quality and veracity.
-          </p>
-          <p className="tracking-wide text-lg md:text-2xl mt-10">
-            This decade is a ‘decade-of-change’ where we decide the trajectory
-            and velocity of the fate of life on our planet.
-          </p>
+        <div className="w-full lg:w-1/2 flex flex-col justify-center items-center">
+          <div className="w-full lg:w-10/12 py-6 md:py-10 lg:py-16 px-4 md:px-10 lg:px-0">
+            <p className="tracking-wide text-lg md:text-2xl">
+              ARROW ACCEL is built on our core principle of placing our
+              clients'​ interests first. Our intrinsic values represent
+              integrity, prudency, tenacity, quality and veracity.
+            </p>
+            <p className="tracking-wide text-lg md:text-2xl mt-10">
+              This decade is a ‘decade-of-change’ where we decide the trajectory
+              and velocity of the fate of life on our planet.
+            </p>
+          </div>
         </div>
-        <div className="w-full lg:w-1/2 flex min-w-min  justify-center items-center px-4 py-4 lg:py-[7rem] lg:px-[10rem] bg-black text-white">
-          <div className="w-1/3 flex flex-col justify-center px-5 items-center">
-            <p className="text-xl md:text-3xl lg:text-5xl font-bold">Small</p>
-            <p className="text-xl md:text-3xl lg:text-5xl font-bold">steps.</p>
-          </div>
-          <div className="w-1/3 flex flex-col justify-center items-center">
-            <img src="/images/logo-icon-white.png" className="w-3/4" />
-          </div>
-          <div className="w-1/3 flex flex-col justify-center items-center px-5">
-            <p className="text-xl md:text-3xl lg:text-5xl font-bold">Giant</p>
-            <p className="text-xl md:text-3xl lg:text-5xl font-bold">leaps.</p>
+        <div className="w-full lg:w-1/2 flex flex-col justify-center items-center px-4 py-4 bg-black text-white">
+          <div className="w-full flex lg:w-10/12 py-6 md:py-10 lg:py-16 px-4 md:px-10 lg:px-0">
+            <div className="w-1/3 flex flex-col justify-center px-5 items-center">
+              <p className="text-xl md:text-3xl lg:text-5xl font-bold">Small</p>
+              <p className="text-xl md:text-3xl lg:text-5xl font-bold">
+                steps.
+              </p>
+            </div>
+            <div className="w-1/3 flex flex-col justify-center items-center">
+              <img src="/images/logo-icon-white.png" className="w-3/4" />
+            </div>
+            <div className="w-1/3 flex flex-col justify-center items-center px-5">
+              <p className="text-xl md:text-3xl lg:text-5xl font-bold">Giant</p>
+              <p className="text-xl md:text-3xl lg:text-5xl font-bold">
+                leaps.
+              </p>
+            </div>
           </div>
         </div>
       </div>
       <div className="w-full flex flex-col lg:flex-row lg:py-[5rem]">
-        <div className="w-full lg:w-1/2">
+        <div className="w-full lg:w-1/2 flex">
           <img src="/images/foundation.png" className="w-full" />
         </div>
-        <div className="w-full lg:w-1/2 px-4 lg:px-[10rem] py-6 lg:py-10">
-          <h2 className="tracking-wide text-lg md:text-3xl uppercase text-center lg:text-left">
-            foundation
-          </h2>
-          <p className="tracking-wide text-lg md:text-2xl mt-4 md:mt-10">
-            A strong, visionary strategy and creative team, directing projects
-            together with specialized execution partners, each operating at the
-            top of their respective fields of expertise.
-          </p>
-          <p className="tracking-wide text-lg md:text-2xl mt-4 md:mt-10">
-            Together we help raise visibility, break into the marketplace and
-            help secure funds for ground-breaking projects, disruptive ideas and
-            companies in the Netherlands and around the globe.
-          </p>
+        <div className="w-full lg:w-1/2 flex flex-col justify-center items-center">
+          <div className="w-full lg:w-10/12 py-6 px-4 md:px-10 lg:px-0">
+            <h2 className="tracking-wide text-lg md:text-3xl uppercase text-center lg:text-left">
+              foundation
+            </h2>
+            <p className="tracking-wide text-lg md:text-2xl mt-4 md:mt-10">
+              A strong, visionary strategy and creative team, directing projects
+              together with specialized execution partners, each operating at
+              the top of their respective fields of expertise.
+            </p>
+            <p className="tracking-wide text-lg md:text-2xl mt-4 md:mt-10">
+              Together we help raise visibility, break into the marketplace and
+              help secure funds for ground-breaking projects, disruptive ideas
+              and companies in the Netherlands and around the globe.
+            </p>
+          </div>
         </div>
       </div>
     </div>

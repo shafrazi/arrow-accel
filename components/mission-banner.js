2ce33a1721c73331ddf2922694420412ffe809d0
shafrazi <shafrazi@live.com>
2022-03-27 15:01:23 +0530

    fix mission banner

--- components/mission-banner.js
+++ components/mission-banner.js
@@ -12,22 +12,24 @@ export default function MissionBanner() {
           world and how to increase awareness
         </p>
       </div>
-      <div className="flex flex-col lg:flex-row w-full">
-        <div className="w-full lg:w-1/2 px-4 md:px-8 lg:px-[10rem] py-10 md:py-16 lg:py-[10rem]">
-          <h2 className="tracking-wide text-lg md:text-3xl uppercase text-center lg:text-left">
-            we are on a mission
-          </h2>
-          <p className="tracking-wide text-lg md:text-2xl mt-10">
-            Our mission is to create, commercialize, and capture strategic value
-            sustainably as we take interest into humanitarian and philanthropic
-            ventures to address global issues across five critical concepts
-            categorized by:
-          </p>
-          <p className="tracking-wide text-base md:text-xl mt-10 text-center lg:text-left">
-            People | Planet | Prosperity | Peace | Partnership
-          </p>
+      <div className="flex flex-col lg:flex-row w-full mt-5 md:mt-10 lg:mt-[5rem]">
+        <div className="w-full lg:w-1/2 flex flex-col justify-center items-center">
+          <div className="w-full lg:w-10/12 py-6 md:py-10 lg:py-16 px-4 md:px-10 lg:px-0">
+            <h2 className="tracking-wide text-lg md:text-3xl uppercase text-center lg:text-left">
+              we are on a mission
+            </h2>
+            <p className="tracking-wide text-lg md:text-2xl mt-10">
+              Our mission is to create, commercialize, and capture strategic
+              value sustainably as we take interest into humanitarian and
+              philanthropic ventures to address global issues across five
+              critical concepts categorized by:
+            </p>
+            <p className="tracking-wide text-base md:text-xl mt-10 text-center lg:text-left">
+              People | Planet | Prosperity | Peace | Partnership
+            </p>
+          </div>
         </div>
-        <div className="w-full lg:w-1/2 py-5 lg:py-12 flex justify-center items-center">
+        <div className="w-full lg:w-1/2 flex">
           <img src="/images/mission.png" className="w-full" />
         </div>
       </div>

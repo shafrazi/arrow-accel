e693eb6fdb792606dfeb3f8115ce5f0bdc15c53a
shafrazi <shafrazi@live.com>
2022-04-02 03:47:12 +0530

    add margin to container

--- components/container.js
+++ components/container.js
@@ -1,6 +1,6 @@
 export default function Container({ children }) {
   return (
-    <div className="flex w-full justify-center items-center">
+    <div className="flex w-full justify-center items-center my-16">
       <div className="w-full flex lg:w-10/12 py-6 md:py-10 lg:py-16 px-4 md:px-10 lg:px-0">
         {children}
       </div>

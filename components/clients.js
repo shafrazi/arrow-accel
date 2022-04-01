761af1dc6db2ac23cc9b32730a2fb85a230c4954
shafrazi <shafrazi@live.com>
2022-04-02 03:37:08 +0530

    add clients component

--- components/clients.js
+++ components/clients.js
@@ -3,7 +3,28 @@ import Container from "./container";
 export default function Clients() {
   return (
     <Container>
-      <p>Hello</p>
+      <div className="hidden md:block w-1/2"></div>
+      <div className="flex flex-col w-full md:w-1/2 justify-center items-center">
+        <div className="w-10/12 flex flex-col md:w-full">
+          <h2 className="text-2xl md:text-xl lg:text-2xl font-bold">
+            Our clients
+          </h2>
+          <p
+            className="text-base md:text-lg lg:text-xl mt-4"
+            style={{ lineHeight: 1.6 }}
+          >
+            We have serviced brands, organizations, start-ups and artists, using
+            insight and creativity to launch ideas across the globe.
+          </p>
+          <p
+            className="text-base md:text-lg lg:text-xl mt-4"
+            style={{ lineHeight: 1.6 }}
+          >
+            Our clients confide in us to translate their vision into action.
+            ARROW ACCEL helps pave the road from concept to creation.
+          </p>
+        </div>
+      </div>
     </Container>
   );
 }
--- components/container.js
+++ components/container.js
@@ -1,7 +1,7 @@
 export default function Container({ children }) {
   return (
     <div className="flex w-full justify-center items-center">
-      <div className="w-full flex flex-col lg:w-10/12 py-6 md:py-10 lg:py-16 px-4 md:px-10 lg:px-0">
+      <div className="w-full flex lg:w-10/12 py-6 md:py-10 lg:py-16 px-4 md:px-10 lg:px-0">
         {children}
       </div>
     </div>

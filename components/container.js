8e8747fb93c3148f3c7c31248cd3e0f4c2a374cb
shafrazi <shafrazi@live.com>
2022-04-04 01:26:04 +0530

    change about what we do component

--- components/container.js
+++ components/container.js
@@ -1,7 +1,12 @@
-export default function Container({ children, backgroundColor, topMargin }) {
+export default function Container({
+  children,
+  backgroundColor,
+  topMargin,
+  verticalPadding,
+}) {
   return (
     <div
-      className={`${backgroundColor} flex w-full justify-center items-center mt-${topMargin}`}
+      className={`${backgroundColor} py-${verticalPadding} flex w-full justify-center items-center mt-${topMargin}`}
     >
       <div className="flex w-10/12 py-4 md:py-8 lg:py-12 px-4 md:px-10 lg:px-0">
         {children}

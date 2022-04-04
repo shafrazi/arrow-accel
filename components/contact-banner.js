5158976a24cdb65f7cbdc7d50cc5a7dca48f71f7
shafrazi <shafrazi@live.com>
2022-04-05 01:46:27 +0530

    change affirm logo and text layout in contact banner

--- components/contact-banner.js
+++ components/contact-banner.js
@@ -6,11 +6,11 @@ export default function ContactBanner() {
       <div className="flex w-full flex-col py-16 md:flex-row">
         <div className="flex flex-col w-full items-center md:items-start md:w-1/2 text-white">
           <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold">
-            Let's work together
+            Let's strategize
           </h2>
           <p className="md:hidden mt-10 text-center">
-            The future holds all potential. Work with us and let's punch te
-            petal to progress together. We have the drive for it!
+            Unlock the future's unlimited potential. Work with us and make your
+            mark.
           </p>
           <div className="flex flex-col w-9/12 md:w-11/12 lg:w-8/12 mt-[6rem] md:mt-[3rem]">
             <button className="w-full text-base md:w-10/12 self-start border-white border-solid border-2 mt-8 text-white uppercase py-2 px-2 rounded tracking-wide">
@@ -22,13 +22,14 @@ export default function ContactBanner() {
           </div>
         </div>
         <div className="hidden md:flex flex-col w-full items-center md:items-start md:w-1/2 text-white">
-          <p
+          <div
             className="mt-10 md:text-lg lg:text-xl"
             style={{ lineHeight: 1.6 }}
           >
-            The future holds all potential. Work with us and let's punch te
-            petal to progress together. We have the drive for it!
-          </p>
+            <p>Unlock the future's unlimited</p>
+            <p>potential. Work with us and </p>
+            <p>make your mark.</p>
+          </div>
         </div>
       </div>
     </Container>

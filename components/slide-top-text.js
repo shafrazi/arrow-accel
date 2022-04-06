31ce8abd4e00467ad1bd87a3edd4218436f747b9
shafrazi <shafrazi@live.com>
2022-04-06 23:49:03 +0530

    change width to full in slide top text in mobile

--- components/slide-top-text.js
+++ components/slide-top-text.js
@@ -5,7 +5,7 @@ export default function SlideTopText({ children }) {
     <div className="w-full h-[45vh] md:h-[40rem] flex flex-col justify-center text-base md:text-2xl">
       <div className="flex w-full">
         <div
-          className="animate__animated animate__fadeInDown animate__slow w-3/4 lg:w-full px-5 md:pl-[3rem] lg:pl-[5rem] tracking-wider leading-[30px] md:leading-loose text-xs md:text-2xl lg:text-[40px]"
+          className="animate__animated animate__fadeInDown animate__slow w-full lg:w-full px-5 md:pl-[3rem] lg:pl-[5rem] tracking-wider leading-[30px] md:leading-loose text-xs md:text-2xl lg:text-[40px]"
           style={{
             fontFamily: "OpenSauceOne",
             lineHeight: 1.6,

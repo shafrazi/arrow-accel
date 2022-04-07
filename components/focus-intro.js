381a7cfa31e952d2e94df9f37dc60228e7aa33f8
shafrazi <shafrazi@live.com>
2022-04-08 00:40:33 +0530

    change font sizes in intro industries

--- components/focus-intro.js
+++ components/focus-intro.js
@@ -21,8 +21,8 @@ export default function FocusIntro() {
         </p>
       </SlideTopText>
 
-      <div className="w-full flex flex-col lg:flex-row bg-black text-white px-10 lg:px-[10rem] py-6 lg:py-[8rem] text-xs lg:text-2xl leading-relaxed">
-        <div className="w-full lg:w-1/2 pt-6 lg:py-0 lg:pr-16">
+      <div className="w-full flex flex-col lg:flex-row bg-black text-white px-10 lg:px-[10rem] lg:py-[8rem] text-xs md:text-lg lg:text-2xl leading-relaxed">
+        <div className="w-full lg:w-1/2 py-6 md:py-12 lg:py-0 lg:pr-16">
           <p className="">
             <span className="font-bold">Transitions</span> have social and
             communicative challenges constituting behavioral change, change
@@ -30,7 +30,7 @@ export default function FocusIntro() {
             seems as opportunities abound.
           </p>
         </div>
-        <div className="w-full lg:w-1/2 pt-6 lg:py-0 lg:pl-16">
+        <div className="w-full lg:w-1/2 py-6 md:py-12 lg:py-0 lg:pl-16">
           <p className="">
             <span className="font-bold">Transformations</span> produce
             real-world outcomes which vary greatly by industry and between
@@ -40,11 +40,12 @@ export default function FocusIntro() {
         </div>
       </div>
       <div className="w-full flex flex-col justify-center items-center py-12 md:py-16 lg:py-[8rem]">
-        <p className="text-xl lg:text-2xl text-center w-full px-10 md:px-0 md:w-1/2 lg:w-1/3 leading-relaxed">
-          To reach your goals, you need to translate your mission effectively,
-          with an understanding of your audience, the and you are building and
-          why the world should pay attention.
-        </p>
+        <div className="text-xs md:text-lg lg:text-2xl text-center w-full px-10 md:px-0 md:w-2/3 lg:w-1/3 leading-relaxed">
+          <p>To reach your goals, you need to translate your</p>
+          <p>mission effectively, with an understanding of</p>
+          <p>your audience, the and you are building and</p>
+          <p>why the world should pay attention.</p>
+        </div>
       </div>
     </div>
   );

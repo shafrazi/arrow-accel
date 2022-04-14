70b481fd6a4aa361ce25acaf730fc1451f33b01f
shafrazi <shafrazi@live.com>
2022-04-15 02:07:58 +0530

    add footer to projects

--- pages/projects.js
+++ pages/projects.js
@@ -5,6 +5,7 @@ import Layout from "../components/layout";
 
 import Footer from "../components/footer";
 import ProjectsHero from "../components/projects-hero";
+import ContactBanner from "../components/contact-banner";
 
 export default function Projects() {
   return (
@@ -15,20 +16,35 @@ export default function Projects() {
       </Head>
       <Header backgroundColor="bg-black" textColor="text-white" />
       <ProjectsHero />
-      <Footer
-        backgroundComponent={
-          <video
-            loop
-            autoPlay
-            muted
-            className="w-full pointer-events-none object-cover h-[55rem]"
-          >
-            <source src="/videos/global-video-low.mp4" type="video/mp4" />
-          </video>
+      <ContactBanner
+        style={{ backgroundColor: "#375aa9" }}
+        backgroundColor=""
+        title="Let's talk"
+        description={
+          <div>
+            <p>Let's have a good talk about your latest</p>
+            <p>venture. Book an exploratory meeting</p>
+            <p>with us and discuss your next moves.</p>
+          </div>
         }
-        imageOverText={"Creation."}
-        topImage="/images/globe-mobile.png"
-        mobileTitleText="Creation."
+        topButtonText="Book appointment"
+        topButtonLink="https://meetings-eu1.hubspot.com/robert-schaeffer"
+        bottomButtonText="call us"
+        bottomButtonLink="tel:0031625283072"
+      />
+      <Footer
+        backgroundComponent={<img src="/images/projects-footer.png" />}
+        imageOverText={<p className="text-black">Ambition.</p>}
+        showIcon={false}
+        fontSize="5vw"
+        topImage="/images/city-mobile.png"
+        mobileTitleText={
+          <p className="text-2xl md:text-5xl">
+            The best way to <br />
+            predict the future <br />
+            is to create it.
+          </p>
+        }
       />
     </Layout>
   );

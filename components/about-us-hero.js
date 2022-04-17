c556e22f77239b9bd1f1bb7ac9fa19f6b6e0d8fc
shafrazi <shafrazi@live.com>
2022-04-18 02:08:49 +0530

    change layout og about us hero

--- components/about-us-hero.js
+++ components/about-us-hero.js
@@ -12,10 +12,10 @@ export default function AboutUsHero() {
       >
         <source src="/videos/about-us-video.mp4" type="video/mp4" />
       </video>
-      <div className="flex w-full md:w-1/2 h-full md:h-[30rem] lg:h-[40rem] md:bg-red-900 md:bg-opacity-25 absolute top-[0rem] md:bottom-[0rem] md:left-[5rem]">
-        <div className="flex flex-col md:justify-center items-center mx-5 marker:md:mx-10 lg:mx-20 mt-4 md:mt-18 lg:mt-36 text-center">
+      <div className="flex w-full md:w-1/2 h-full md:h-[30rem] lg:h-[40rem] md:bg-opacity-25 absolute top-[0rem] md:bottom-[0rem] md:left-[5rem]">
+        <div className="flex flex-col md:justify-center items-center mx-5 marker:md:mx-10 lg:mx-15 mt-4 md:mt-18 lg:mt-36 text-center">
           <p
-            className="py-[6rem] md:py-0 tracking-wide text-base md:text-xl lg:text-2xl text-left md:text-center font-semibold text-white mb-2 md:mb-5 lg:mb-10"
+            className="py-[6rem] md:py-0 tracking-wide text-base md:text-xl lg:text-2xl text-left font-semibold text-white mb-2 md:mb-5 lg:mb-10"
             style={{ fontFamily: "OpenSauceOne", fontWeight: "normal" }}
           >
             We are consultants for brands, entertainment professionals,

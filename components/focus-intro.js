a9ad139b3b138b006da6bd22dd3cc70886c79e12
shafrazi <shafrazi@live.com>
2022-04-26 02:08:37 +0530

    add parallax to causes

--- components/focus-intro.js
+++ components/focus-intro.js
@@ -30,21 +30,21 @@ export default function FocusIntro() {
           </p>
         </div>
       </SlideTopText>
-      {/* <div
+      <div
         className="w-full flex flex-col justify-center items-center text-white py-12 md:py-16 lg:py-[8rem]"
         style={{ backgroundColor: "#375aa9" }}
       >
         <div className="text-xs md:text-lg lg:text-2xl text-center w-full px-6 md:px-0 md:w-2/3 leading-relaxed">
           <p>
-            Work with us to identify your goals, to clarify the challenges ahead
-            and to strategize on solutions.
+            Work with us to identify your goals, to clarify the <br />
+            challenges ahead and to strategize on solutions.
           </p>
           <p className="mt-6 md:mt-10">
-            Free yourself and find your flow to move forward frictionless with
-            first steps and renewed energy.
+            Free yourself and find your flow to move forward <br />
+            frictionless with first steps and renewed energy.
           </p>
         </div>
-      </div> */}
+      </div>
 
       {/* <img
           src="/images/build.png"
@@ -60,10 +60,15 @@ export default function FocusIntro() {
           <div style={{ height: 500 }}></div>
         </div>
       </Parallax> */}
-      <div className="w-full h-[50vh]">
+      <div className="w-full">
         <ParallaxBanner
-          layers={[{ image: "/images/build.png", speed: -20 }]}
-          className="h-[50vh] md:h-[100%]"
+          layers={[
+            {
+              image: "/images/build.png",
+              speed: -20,
+            },
+          ]}
+          className="w-[100%] h-[50vh] lg:h-[500px]"
         />
       </div>
     </div>
--- pages/causes.js
+++ pages/causes.js
@@ -6,56 +6,53 @@ import FocusIntro from "../components/focus-intro";
 import BuildIt from "../components/build-it";
 import ContactBanner from "../components/contact-banner";
 import OurFuture from "../components/our-future";
-import { ParallaxProvider } from "react-scroll-parallax";
 
 export default function Causes() {
   return (
-    <ParallaxProvider>
-      <Layout>
-        <Head>
-          <title>Causes | Arrow Accel</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <Header />
-        <FocusIntro />
-        <BuildIt />
-        <ContactBanner
-          title="Let's talk"
-          description={
-            <div>
-              <p>
-                Let's have a good talk about your latest venture. Book an
-                exploratory meeting with us and discuss your next moves.
-              </p>
-            </div>
-          }
-          topButtonText="Book appointment"
-          topButtonLink="https://meetings-eu1.hubspot.com/robert-schaeffer"
-          bottomButtonText="call us"
-          bottomButtonLink="tel:0031625283072"
-        />
-        <OurFuture />
-        <Footer
-          backgroundComponent={<img src="/images/causes-footer.png" />}
-          imageOverText={
-            <p className="text-2xl md:text-4xl">
-              The best way to <br />
-              predict the future <br />
-              is to create it.
+    <Layout>
+      <Head>
+        <title>Causes | Arrow Accel</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <FocusIntro />
+      <BuildIt />
+      <ContactBanner
+        title="Let's talk"
+        description={
+          <div>
+            <p>
+              Let's have a good talk about your latest venture. Book an
+              exploratory meeting with us and discuss your next moves.
             </p>
-          }
-          showIcon={false}
-          fontSize="3vw"
-          topImage="/images/city-mobile.png"
-          mobileTitleText={
-            <p className="text-2xl md:text-5xl">
-              The best way to <br />
-              predict the future <br />
-              is to create it.
-            </p>
-          }
-        />
-      </Layout>
-    </ParallaxProvider>
+          </div>
+        }
+        topButtonText="Book appointment"
+        topButtonLink="https://meetings-eu1.hubspot.com/robert-schaeffer"
+        bottomButtonText="call us"
+        bottomButtonLink="tel:0031625283072"
+      />
+      <OurFuture />
+      <Footer
+        backgroundComponent={<img src="/images/causes-footer.png" />}
+        imageOverText={
+          <p className="text-2xl md:text-4xl">
+            The best way to <br />
+            predict the future <br />
+            is to create it.
+          </p>
+        }
+        showIcon={false}
+        fontSize="3vw"
+        topImage="/images/city-mobile.png"
+        mobileTitleText={
+          <p className="text-2xl md:text-5xl">
+            The best way to <br />
+            predict the future <br />
+            is to create it.
+          </p>
+        }
+      />
+    </Layout>
   );
 }

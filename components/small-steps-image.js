063d11a1393ae5246972831ea44b723ccc2a59ac
shafrazi <shafrazi@live.com>
2022-08-06 00:31:08 +0530

    add parallax to home page image

--- components/small-steps-image.js
+++ components/small-steps-image.js
@@ -1,14 +1,24 @@
 import LazyLoad from "react-lazyload";
+import { ParallaxBanner } from "react-scroll-parallax";
 
 export default function SmallStepsImage() {
   return (
     <div className="w-full bg-gradient-to-t to-white relative">
-      <LazyLoad>
+      {/* <LazyLoad>
         <img
           src="/images/woman-looking.png"
           className="w-full h-[50vh] md:h-full object-cover"
         />
-      </LazyLoad>
+      </LazyLoad> */}
+      <ParallaxBanner
+        layers={[
+          {
+            image: "/images/woman-looking.png",
+            speed: -20,
+          },
+        ]}
+        className="w-full h-[20vh] md:h-[50vh] lg:h-[500px] object-cover"
+      />
     </div>
   );
 }
